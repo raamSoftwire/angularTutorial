cd8e8998620e46178433dbba2ee2175af29a9349
raamSoftwire <raam.chauhan@softwire.com>
2018-03-16 16:16:07 +0000

    Tutorial stages 1-4

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Steve the Coding Werewolf' },
+  { id: 12, name: 'Dank Queen Lizzy' },
+  { id: 13, name: 'peggy5' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
