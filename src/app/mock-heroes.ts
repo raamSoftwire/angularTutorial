4e52e2f2871f75131c1b41989488da069a7d0849
raamSoftwire <raam.chauhan@softwire.com>
2018-03-19 12:17:49 +0000

    Add steps 5 and 6 of tutorial - Components and Services

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -4,7 +4,7 @@ export const HEROES: Hero[] = [
   { id: 11, name: 'Steve the Coding Werewolf' },
   { id: 12, name: 'Dank Queen Lizzy' },
   { id: 13, name: 'peggy5' },
-  { id: 14, name: 'Celeritas' },
+  { id: 14, name: 'Morale Officer' },
   { id: 15, name: 'Magneta' },
   { id: 16, name: 'RubberMan' },
   { id: 17, name: 'Dynama' },
